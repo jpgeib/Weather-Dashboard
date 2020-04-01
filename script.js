6a9075108394304ca7f22ccf0449d3255d61a98c
James Geib <jpgeib2@gmail.com>
2020-04-01 16:22:28 -0700

    everything is good to go

--- script.js
+++ script.js
@@ -7,14 +7,17 @@ $("#currentDate").text(moment().format('MMMM Do' + ',' + ' YYYY'));
 const apiKey = "f04e167b5e7bea9405c4b7431c3910b6"
 
 const searchWeather = function(city) {
+    $("#temperature").empty();
+    $("#humidity").empty();
+    $("#windSpeed").empty();
     $.ajax({
         type: "GET",
         url: "http://api.openweathermap.org/data/2.5/weather?q=" + city + " &appid=" + apiKey + "&units=imperial",
         dataType: "JSON",
         success: function(data) {
-            $("#temperature").append(data.main.temp + " °F");
-            $("#humidity").append(data.main.humidity + " %");
-            $("#windSpeed").append(data.wind.speed + " mph");
+            $("#temperature").append("Temperature: " + data.main.temp + " °F");
+            $("#humidity").append("Humidity: " + data.main.humidity + " %");
+            $("#windSpeed").append("Wind Speed: " + data.wind.speed + " mph");
             console.log(data);
             //Call UV ajax function for longitude and latitude
             console.log("UV", data.coord.lat, data.coord.lon)
@@ -26,12 +29,20 @@ const searchWeather = function(city) {
 
 //Make separate ajax call for UV index
 const getUVIndex = function(lat, lon) {
+    $("#UV").empty();
     $.ajax({
         type: "GET",
         url: "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey + "&units=imperial",
         dataType: "JSON",
         success: function(data) {
-            $("#UV").append("<span id='uvindex'>" + data.value + "</span>");
+            $("#UV").append("UV Index: " + "<span id='uvindex'>" +  data.value + "</span>");
+            if(data.value < 3) {
+                $("#uvindex").addClass("btn-success"); 
+            } else if(data.value < 7) {
+                $("#uvindex").addClass("btn-warning");
+            } else {
+                $("#uvindex").addClass("btn-danger");
+            }
             console.log("UV Index: ", data.value, " for", lat, "& ", lon);
         }
     });
@@ -39,23 +50,59 @@ const getUVIndex = function(lat, lon) {
 //Make forecast ajax call
 
 const getForecast = function(city) {
+    $("#monday").empty();
+    $("#tuesday").empty();
+    $("#wednesday").empty();
+    $("#thursday").empty();
+    $("#friday").empty();
+
     $.ajax({
         type: "GET",
-        url: "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey,
+        url: "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey + "&units=imperial",
         dataType: "JSON",
         success: function(data) {
-            for(let i = 0; i < data.list.length; i++) {
-                $("monday").append(data.list[0].weather[0].main);
-                $("tuesday").append(data.list[8].weather[0].main);
-                // $("wednesday").append(data.list[2].weather[2].main);
-                // $("thursday").append(data.list[3].weather[3].main);
-                // $("friday").append(data.list[4].weather[4].main);
-                console.log(data.list.weather[0].main);
-                // console.log(data.list[1].weather[1].main);
-                // console.log(data.list[2].weather[2].main);
-                // console.log(data.list[3].weather[3].main);
-                // console.log(data.list[4].weather[4].main);
-            }
+
+            
+                //Card title dates
+                $("#monday").append(new Date(data.list[0].dt_txt).toLocaleDateString() + "<br>");
+                $("#tuesday").append(new Date(data.list[8].dt_txt).toLocaleDateString() + "<br>");
+                $("#wednesday").append(new Date(data.list[17].dt_txt).toLocaleDateString() + "<br>");
+                $("#thursday").append(new Date(data.list[25].dt_txt).toLocaleDateString() + "<br>");
+                $("#friday").append(new Date(data.list[33].dt_txt).toLocaleDateString() + "<br>");
+
+                //Weather description forecast
+                $("#monday").append(data.list[0].weather[0].main + "<br>");
+                $("#tuesday").append(data.list[8].weather[0].main + "<br>");
+                $("#wednesday").append(data.list[17].weather[0].main + "<br>");
+                $("#thursday").append(data.list[25].weather[0].main + "<br>");
+                $("#friday").append(data.list[33].weather[0].main + "<br>");
+                
+                //Temperature forecast
+                $("#monday").append(data.list[0].main.temp + " °F" + "<br>");
+                $("#tuesday").append(data.list[8].main.temp + " °F" + "<br>");
+                $("#wednesday").append(data.list[17].main.temp + " °F" + "<br>");
+                $("#thursday").append(data.list[25].main.temp + " °F" + "<br>");
+                $("#friday").append(data.list[33].main.temp + " °F" + "<br>");
+
+                //Humidity forecast
+                $("#monday").append(data.list[0].main.humidity + " %" + "<br>");
+                $("#tuesday").append(data.list[8].main.humidity + " %" + "<br>");
+                $("#wednesday").append(data.list[17].main.humidity + " %" + "<br>");
+                $("#thursday").append(data.list[25].main.humidity + " %" + "<br>");
+                $("#friday").append(data.list[33].main.humidity + " %" + "<br>");
+
+                //Weather icons
+                var img = $("<img>").attr("src", "http://openweathermap.org/img/w/" + data.list[0].weather[0].icon + ".png");
+                $("#monday").append(img);
+                img = $("<img>").attr("src", "http://openweathermap.org/img/w/" + data.list[8].weather[0].icon + ".png");
+                $("#tuesday").append(img);
+                img = $("<img>").attr("src", "http://openweathermap.org/img/w/" + data.list[17].weather[0].icon + ".png");
+                $("#wednesday").append(img);
+                img = $("<img>").attr("src", "http://openweathermap.org/img/w/" + data.list[25].weather[0].icon + ".png");
+                $("#thursday").append(img);
+                img = $("<img>").attr("src", "http://openweathermap.org/img/w/" + data.list[33].weather[0].icon + ".png");
+                $("#friday").append(img);
+
         }
     });
 };
